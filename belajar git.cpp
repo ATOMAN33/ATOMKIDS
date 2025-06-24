36c86336b38cb5d4fa69b76f64c530f4ae866d1d
K!? <mahardikam789@gmail.com>
2025-06-24 15:58:38 +0700

    Update belajar git.cpp

--- belajar git.cpp
+++ belajar git.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
-using namespace std;
-
-int main () {
-    cout >> "apa";
-}
+Arkan adalah seorang mahasiswa informatika yang sedang mengembangkan sebuah sistem sederhana untuk membatu pengelolaan data mahasiswa di kampusnya. dalam sistem tersebut, arkan ingin menyimpan infrmasi dasar mahasiswa seperti:
+	- Nomor induk Mahasiswa (NIM)
+	- Nama
+	- Program Studi (prodi)
+	- Nomor telepon
+	
+	namun, dalam proses pengembangannya, arkan menghadapi beberapa kendala, yaitu:
+		1. Kesulitan mencari data mahasiswa berdasarkan NIM secara cepat dan akurat.
+		2. Urutkan berdasarkan ascending dan descending
+	
+menggunakan coding c++, menggunakan class
